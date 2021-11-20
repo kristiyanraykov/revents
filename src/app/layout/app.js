351c125c9dc84fd42deb5797258f8d7541612120
Kristiyan Raykov <craykov@yahoo.com>
2021-11-20 18:13:09 +0200

    initialized project

---
+++ src/app/layout/app.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Re-Vents</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+const rootEl = document.getElementById('root');
+function render() {
+  ReactDOM.render(<App />, rootEl)
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function(){
+    setTimeout(render)
+  })
+}
+
+render()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
